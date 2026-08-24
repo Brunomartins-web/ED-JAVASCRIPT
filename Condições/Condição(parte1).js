7cacd59f4139ab7826c8afd6f94344889cbd9c98
Bruno Martins <brusk8403@gmail.com>
2026-08-24 18:24:43 -0300

    Aula de condições!

---
+++ Condições/Condição(parte1).js
@@ -0,0 +1,7 @@
+let vel = 62
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60){ // CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`Direja sempre usando cinto de segurança`)
